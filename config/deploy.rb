c70ebc9f50940a84a4c865d1601e768f16c80552
kyary4645 <kyary.0426@gmail.com>
2019-12-05 11:51:28 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,6 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
-set :linked_files, %w{ config/master.key }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
