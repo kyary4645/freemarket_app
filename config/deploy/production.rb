b411197c47326f9f291688c49542653d8759ee2e
kyary4645 <kyary.0426@gmail.com>
2019-12-05 15:42:24 +0900

    Basic認証の設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,8 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '18.177.66.99', user: 'ec2-user', roles: %w{app db web}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
