acd834c477a7373d6be01009d9b344d22c5abcda
kyary4645 <kyary.0426@gmail.com>
2019-12-11 16:49:21 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
+
+  resources :items, only: [:index, :new, :create, :destroy, :show]
+  resources :users do
+    collection do
+      get 'index'
+      get 'step1'
+      post 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'done'
+    end
+  end
 end
