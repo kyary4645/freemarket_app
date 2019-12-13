5a07e09b54fb1554fded6a080ded08e18fa2bce9
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2019-12-13 14:45:14 +0900

    商品出品
    商品出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,48 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+    
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new, notice: '保存できませんでした'
+    end
+  end
+
+
+  def destroy
+    item = Item.find(params[:id])
+
+  end  
+
+  def edit
+  end  
+
+  def show
+  end
+
+  
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :size, :status, :derivery_fee, :derivery_method,
+      :price, :derivery_estimated, :description, :image, :category_id, :brand_id, :prefecture_id)
+  end  
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+
+  validates :name,:status,:derivery_fee,:price,:derivery_estimated,:description,:image,:category_id,:prefecture_id, presence: true
+  
+  mount_uploader :image, ImageUploader
+
 end
