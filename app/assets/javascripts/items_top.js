019d91105045aa205078cf1be3f3eff977deb095
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2019-12-13 20:39:39 +0900

    toppage　スライドショー
    トップページ広告部分のスライドショー

---
+++ app/assets/javascripts/items_top.js
@@ -0,0 +1,28 @@
+
+
+$(function() {
+  $('.advertisement-box').slick({
+      dots: true,
+      // 下のドット表示
+      
+
+      autoplay: true,
+      // 自動再生。trueで自動再生される。
+      speed: 400,
+      // 自動再生や左右の矢印でスライドするスピード
+      autoplaySpeed: 3000,
+      // 自動再生で切り替えをする時間
+      accessibility: true,
+      // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
+  });
+});
+
+
+
+
+
+
+
+
+
+
