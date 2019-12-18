64affc7d93c9194a2a595bfa69178a406d58485e
apple-coffee <conpota8649.cp@icloud.com>
2019-12-18 12:11:18 +0900

    purchase-controller delete

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,5 @@ class Item < ApplicationRecord
 
   mount_uploader :image, ImageUploader
   belongs_to :user
-
+  has_one :purchase
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,7 @@ Rails.application.routes.draw do
   # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :destroy, :show] do
-    resources :purchase, only: [:index, :done]
-  end
-
+  resources :items, only: [:index, :new, :create, :destroy, :show]
 
 
   resources :users do
