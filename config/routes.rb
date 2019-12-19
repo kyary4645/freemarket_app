036b0bdcd25d026dc0d7bd7504bd0737308b2cef
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2019-12-19 20:22:28 +0900

    update
    修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   }
   
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :destroy, :show, :edit] do
+  resources :items, only: [:index, :new, :create, :destroy, :show, :edit,:update] do
     resources :purchase, only: [:show] do
       member do
         post 'pay', to: 'purchase#pay'
