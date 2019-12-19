fb752c28c7fb7829a51d6ad034fc1034560e2a0b
kazoo47 <tanabata47@gmail.com>
2019-12-19 10:36:37 +0900

    表記変更

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,16 +1,16 @@
 
-# # ルート
-# crumb :root do
-#   link "トップページ", root_path
-# end
+# ルート
+crumb :root do
+  link "トップページ", root_path
+end
 
-# # マイページ
-# crumb :users do
-#   link "マイページ", user_path(current_user.id)
-#   parent :root
-# end
+# マイページ
+crumb :users do
+  link "マイページ", user_path(current_user.id)
+  parent :root
+end
 
-# プロフィール
+プロフィール
 
 crumb :"/users/index/profile" do
   link "プロフィール", profile_user_path(current_user.id)
