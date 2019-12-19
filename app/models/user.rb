c8d2f4a63889b6070d52c30ad3ba3b74705a17e7
kyary4645 <kyary.0426@gmail.com>
2019-12-19 14:26:41 +0900

    whereをfind_byに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
 
 
   def self.without_sns_data(auth)
-    user = User.where(email: auth.info.email).first
+    user = User.find_by(email: auth.info.email)
 
     if user.present?
       sns = SnsCredential.create(
@@ -42,7 +42,7 @@ class User < ApplicationRecord
 
   def self.with_sns_data(auth, snscredential)
 
-    user = User.where(id: snscredential.user_id).first
+    user = User.find_by(id: snscredential.user_id)
 
     unless user.present?
       user = User.new(
@@ -58,7 +58,7 @@ class User < ApplicationRecord
   def self.find_oauth(auth)
     uid = auth.uid
     provider = auth.provider
-    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    snscredential = SnsCredential.find_by(uid: uid, provider: provider)
     
     if snscredential.present?
       user = with_sns_data(auth, snscredential)[:user]
