ff40c472c042953e4e5cf3674807b6aa9544c58e
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2019-12-19 12:13:46 +0900

    パンくず設定
    パンくず設定

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -16,9 +16,27 @@ crumb :"/users/index/profile" do
   parent :users
 end
 
-# # # 出品した商品
-# crumb :"/users/index/tab" do
-#   link "出品した商品", tab1_user_path(current_user.id)
-#   parent :"/users/index/profile"
-# end
+# # 本人情報
+crumb :"/users/index/personalinformation" do
+  link "本人情報", personalinformation_user_path(current_user.id)
+  parent :users
+end
 
+# # 出品中
+crumb :"/users/index/tab1" do
+  link "出品中", tab1_user_path(current_user.id)
+  parent :users
+end
+
+# # 取引中
+crumb :"/users/index/tab2" do
+  link "取引中", tab2_user_path(current_user.id)
+  parent :users
+end
+
+
+# # 売却済
+crumb :"/users/index/tab3" do
+  link "売却済", tab3_user_path(current_user.id)
+  parent :users
+end
